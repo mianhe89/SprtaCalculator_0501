1a2537c0c1458c5ea3b6b62c7f5b019054fa1d1b
Robbie <robbie331@>
2024-05-01 13:35:38 +0900

    Level1 - 3 simpleCalculator

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -6,19 +6,37 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        //1
-        System.out.print("첫 번째 숫자를 입력하세요: ");
-        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-        int num1 = sc.nextInt();
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        int num2 = sc.nextInt();
-        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-        System.out.println("num1 = " + num1 + " num2 = " + num2);
+//        //1
+//        System.out.print("첫 번째 숫자를 입력하세요: ");
+//        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+//        int num1 = sc.nextInt();
+//        System.out.print("두 번째 숫자를 입력하세요: ");
+//        int num2 = sc.nextInt();
+//        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+//        System.out.println("num1 = " + num1 + " num2 = " + num2);
+//
+//        //2
+//        System.out.println("사칙연산 기호를 입력하세요: "); // + - * /
+//        char operator = sc.nextLine().charAt(0); //charAt은 String으로 저장된 문자열 중에서 한 인덱스만 선택해서 char타입으로 변환
+//        // next nextLine -> String 타입으로 입력 받음
+//        System.out.println("operator = " + operator);
 
-        //2
-        System.out.println("사칙연산 기호를 입력하세요: "); // + - * /
-        char operator = sc.nextLine().charAt(0); //charAt은 String으로 저장된 문자열 중에서 한 인덱스만 선택해서 char타입으로 변환
-        // next nextLine -> String 타입으로 입력 받음
-        System.out.println("operator = " + operator);
+        //3
+        System.out.println("사칙연산 + - / * % 중에 입력해주세요.");
+        String operator = sc.nextLine();
+        System.out.println("첫번째 숫자를 입력해주세요.");
+        int preNum = sc.nextInt();
+        System.out.println("두번째 숫자를 입력해주세요.");
+        int nextNum = sc.nextInt();
+        double resultNum = switch (operator) {
+            case "+" -> preNum + nextNum;
+            case "-" -> preNum - nextNum;
+            case "*" -> preNum * nextNum;
+            case "/" -> (double) preNum / nextNum;
+            case "%" -> preNum % nextNum;
+            default -> 0;
+        };
+        System.out.println("결과값 = "+ resultNum);
+
     }
 }
